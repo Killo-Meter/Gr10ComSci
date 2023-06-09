3887001b49ee4ac45f9b2f2173884f4f002f6c15
Killo-Meter <135991568+Killo-Meter@users.noreply.github.com>
2023-06-09 11:35:01 -0400

    Add files via upload

--- ISU.js
+++ ISU.js
@@ -1,14 +1,47 @@
 var output = "";
 var sum = document.getElementById("equal")
+var toggle = 0;
+var operator;
+var operand1;
+var operand2 = "";
 
-function modOutput(value) {
-	output += value;
-	document.getElementById("sum").innerHTML = output;
-}
 function clearCalculator() {
 	document.getElementById("sum").innerHTML = "";
 	output = "";
+	operand2 = "";
+	operand1 = "";
+	toggle = 0;
 }
-function calculate(){
-	
+function numberPress(value){
+	output+= value;
+	document.getElementById("sum").innerHTML = output;
+	if (toggle == 1) {
+		operand2 += value;
+	}
+}
+function operatorPress(value) {
+	operand1 = output;
+	operator = value;
+	output+= value;
+	document.getElementById("sum").innerHTML = output;
+	toggle = 1;
+}
+
+function calculate() {
+if (operator == '+'){
+	document.getElementById("sum").innerHTML = Number.parseInt(operand1) + Number.parseInt(operand2);
+}
+if (operator == '-'){
+	document.getElementById("sum").innerHTML = Number.parseInt(operand1) - Number.parseInt(operand2);
+}
+if (operator == 'x'){
+	document.getElementById("sum").innerHTML = Number.parseInt(operand1) * Number.parseInt(operand2);
+}
+if (operator == '÷'){
+	document.getElementById("sum").innerHTML = Number.parseInt(operand1) / Number.parseInt(operand2);
+}
+	operand2 = "";
+	operand1 = "";
+	toggle = 0;
+	output = "";
 }
